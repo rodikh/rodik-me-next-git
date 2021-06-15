02d24a6bf55ef3d6fa6648fc45e50660984eac8b
Stackbit Code Editor <projects@stackbit.com>
2021-06-15 17:13:04 +0000

    ParticleLinesSection.js: changed

--- src/components/ParticleLinesSection.js
+++ src/components/ParticleLinesSection.js
@@ -9,13 +9,14 @@ export default class ParticleLinesSection extends React.Component {
         super(props);
         this.canvasRef = React.createRef();
     }
-    
+
     componentDidMount() {
         const section = _.get(this.props, 'section');
 
         this.canvasRef.current.width = this.canvasRef.current.parentNode.clientWidth;
         this.canvasRef.current.height = 600;
         new ParticleLines(this.canvasRef.current, {particlesAmount: section.options.particle_count, maxDistance: 150});
+        
     }
 
     render() {
