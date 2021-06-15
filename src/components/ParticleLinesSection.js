765fc1848f2f6a22cb12cec4f36390d3067afec2
Stackbit Code Editor <projects@stackbit.com>
2021-06-15 12:28:34 +0000

    ParticleLinesSection.js: changed

--- src/components/ParticleLinesSection.js
+++ src/components/ParticleLinesSection.js
@@ -21,7 +21,7 @@ export default class ParticleLinesSection extends React.Component {
         
         return (
             <div className="embed-block container container--md">
-                <canvas id={section.canvas_id} ref={this.canvasRef} />
+                <canvas className="accent-box" id={section.canvas_id} ref={this.canvasRef} />
             </div>
 
         );
