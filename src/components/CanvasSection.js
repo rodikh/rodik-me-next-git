ddd8180039810f8ad8e089d5f09a9885018a7644
Stackbit Code Editor <projects@stackbit.com>
2021-06-15 18:32:31 +0000

    CanvasSection.js: changed

--- src/components/CanvasSection.js
+++ src/components/CanvasSection.js
@@ -15,7 +15,7 @@ export default class CanvasSection extends React.Component {
 
     componentDidMount() {
         const section = _.get(this.props, 'section');
-        const demoType = section.demoType;
+        const demoType = section.demo_type;
 
         this.canvasRef.current.width = this.canvasRef.current.parentNode.clientWidth;
         this.canvasRef.current.height = 600;
