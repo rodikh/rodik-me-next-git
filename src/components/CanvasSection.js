18555f0e8c943b15138481d67c0f2ba9ae5a17ab
Stackbit Code Editor <projects@stackbit.com>
2021-06-15 18:44:06 +0000

    CanvasSection.js: changed

--- src/components/CanvasSection.js
+++ src/components/CanvasSection.js
@@ -39,6 +39,7 @@ export default class CanvasSection extends React.Component {
     demoTypes = {
         'particle-lines': {
             up: (canvas, options) => {
+                console.log('running up', canvas, options);
                 const engine = new ParticleEngine(canvas, {color: '0,0,0', particleLines: true, particlesAmount: options.particle_count, maxLineDistance: 150});
                 this.drawQueue.push(engine);
             },
